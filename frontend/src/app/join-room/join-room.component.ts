95c834d313164e80b4639ef5eb6aaf0652b115fa
PiotrSzalski <szalski.piotr@gmail.com>
2019-11-30 17:56:50 +0100

    User has list of latest joined rooms

--- frontend/src/app/join-room/join-room.component.ts
+++ frontend/src/app/join-room/join-room.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { BackendConnectionService } from "../backend-connection.service";
 import { Router, ActivatedRoute } from '@angular/router';
+import { Room } from '../room';
+import { Title } from "@angular/platform-browser";
 
 @Component({
   selector: 'app-join-room',
@@ -10,21 +12,47 @@ import { Router, ActivatedRoute } from '@angular/router';
 export class JoinRoomComponent implements OnInit {
 
   private shortLink: string;
+  private array: Array<Room>
 
-  constructor(private backendService: BackendConnectionService, private router: Router, private route: ActivatedRoute) { }
+  constructor(private backendService: BackendConnectionService, 
+    private router: Router, 
+    private route: ActivatedRoute,
+    private titleService: Title) { }
 
   ngOnInit() {
+    this.titleService.setTitle( "Instant Polls - Dołącz do pokoju" );
     this.shortLink = "";
     this.route.params.subscribe(params => {
       if(params['shortLink'] !== undefined) {
+        console.log("asd");
         this.backendService.getLongId(params['shortLink']).subscribe(response => {
           var roomId = response['roomId'];
           if(roomId !== null) {
+            var storedRooms = JSON.parse(localStorage.getItem("latests"));
+            if(!storedRooms.includes(roomId)) {
+              storedRooms.push(roomId);
+              localStorage.setItem("latests", JSON.stringify(storedRooms));
+            }
             this.router.navigate(['/pollroom/', roomId]);
           } else {
             this.router.navigate(['/join']);
           }
         });
+      }
+      if (localStorage.getItem("latests") !== null) {
+        this.backendService.checkUserRoom(localStorage.getItem("latests")).subscribe(response => {
+          this.array = response;
+          var rooms = [];
+          for (var i in response) {
+            rooms.push(response[i].id);
+            if(rooms.length > 5) {
+              rooms.shift();
+            }
+          }
+          localStorage.setItem("latests", JSON.stringify(rooms));
+        });
+      } else {
+        localStorage.setItem("latests", JSON.stringify([]));
       }
     });
   }
@@ -34,6 +62,11 @@ export class JoinRoomComponent implements OnInit {
         this.backendService.getLongId(this.shortLink).subscribe(response => {
           var roomId = response['roomId'];
           if(roomId !== null) {
+            var storedRooms = Array.from(JSON.parse(localStorage.getItem("latests")));
+            if(!storedRooms.includes(roomId)) {
+              storedRooms.push(roomId);
+              localStorage.setItem("latests", JSON.stringify(storedRooms));
+            }
             this.router.navigate(['/pollroom/', roomId]);
           }
         });
